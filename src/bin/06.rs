b6b90d19dce0b8637130af5bb18dc7441c4b6214
Peter Cheng <pcheng17@gmail.com>
2023-01-06 13:19:25 -0800

    Improve day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,38 +1,40 @@
-use itertools::Itertools;
-
-fn parse(input: &str) -> Vec<char> {
-    input.chars().collect()
+fn parse(input: &str) -> &[u8] {
+    input.as_bytes()
 }
 
-fn find_marker(chars: &[char], window_size: usize) -> Option<usize> {
-    chars
+fn find_marker(input: &[u8], window_size: usize) -> Option<usize> {
+    let mut mask = 0u32;
+    input
+        .iter()
+        .take(window_size - 1)
+        .for_each(|c| mask ^= 1 << (c % 32));
+
+    input
         .windows(window_size)
-        .find_position(|window| {
-            for (i, x) in window.iter().enumerate() {
-                for (j, y) in window.iter().enumerate() {
-                    if i != j && x == y {
-                        return false;
-                    }
-                }
-            }
-            true
+        .position(|w| {
+            let first = w[0];
+            let last = w[w.len() - 1];
+            mask ^= 1 << (last % 32);
+            let res = mask.count_ones() == window_size as u32;
+            mask ^= 1 << (first % 32);
+            res
         })
-        .map(|(i, _)| i + window_size)
+        .map(|i| i + window_size)
 }
 
-pub fn part_one(input: &[char]) -> Option<usize> {
+pub fn part_one(input: &[u8]) -> Option<usize> {
     find_marker(input, 4)
 }
 
-pub fn part_two(input: &[char]) -> Option<usize> {
+pub fn part_two(input: &[u8]) -> Option<usize> {
     find_marker(input, 14)
 }
 
 fn main() {
     let input = advent_of_code::read_file("inputs", 6);
     let parsed = advent_of_code::run_parser(parse, &input);
-    advent_of_code::solve!(1, part_one, &parsed);
-    advent_of_code::solve!(2, part_two, &parsed);
+    advent_of_code::solve!(1, part_one, parsed);
+    advent_of_code::solve!(2, part_two, parsed);
 }
 
 #[cfg(test)]
